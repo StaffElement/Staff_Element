db921a972b582f85260128cda35b61c27bb9a726
ScottXileChen <53885585+ScottXileChen@users.noreply.github.com>
2020-11-20 21:09:05 +0800

    Xile Chen (#3)
    Added basic input system. Added Player move and jump classes.

---
+++ StaffElement/Assets/Scripts/Player/InputSystem.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputSystem : MonoBehaviour
+{
+    public event Action<float> OnMoveButtonPressed; 
+    public event Action<float> OnJumpButtonPressed;
+    public event Action OnLeftClick;
+    public event Action OnRightClick;
+
+    private void Update()
+    {
+        // Left Click event:
+        if (Input.GetMouseButtonDown(0)) OnLeftClick?.Invoke();
+
+        // Right Click event:
+        if (Input.GetMouseButtonDown(2)) OnRightClick?.Invoke();
+    }
+
+    private void FixedUpdate()
+    {
+        // Moving event:
+        if (Input.GetAxis("Horizontal") != 0) OnMoveButtonPressed?.Invoke(Input.GetAxis("Horizontal"));
+
+        // Jumping event:
+        if (Input.GetAxis("Jump") != 0) OnJumpButtonPressed?.Invoke(Input.GetAxis("Horizontal"));
+    }
+}
---
+++ StaffElement/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(InputSystem))]
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMovement : MonoBehaviour
+{
+    public event Action OnMoving;
+    public event Action OnJumping;
+
+    [SerializeField]
+    private float moveSpeed = 0f;
+    [SerializeField]
+    private float jumpForce = 0f;
+
+    private Rigidbody2D rb;
+    private InputSystem inputSystem;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        inputSystem = GetComponent<InputSystem>();
+
+        inputSystem.OnMoveButtonPressed += (inputAxis) => Moving(inputAxis);
+        inputSystem.OnJumpButtonPressed += (inputAxis) => Jumping(inputAxis);
+    }
+
+    private void Moving(float inputAxis)
+    {
+        // TODO: Moving Implement Here
+        OnMoving?.Invoke();
+    }
+
+    private void Jumping(float inputAxis)
+    {
+        // TODO: Jumping Implement Here
+        OnJumping?.Invoke();
+    }
+}
