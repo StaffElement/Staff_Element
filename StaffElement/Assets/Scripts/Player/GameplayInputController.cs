25ef4fb287cf0f93a732d23d3a2d5e14113af842
Chenpochun98 <61893568+Chenpochun98@users.noreply.github.com>
2020-11-24 03:32:29 -0800

    Jump2 (#8)
    * Pochu Chen
    
    Test
    
    * Pochun Chen
    
    1.fix jump function
    2.inputsystem movement and jump put in update function

--- StaffElement/Assets/Scripts/Player/GameplayInputController.cs
+++ StaffElement/Assets/Scripts/Player/GameplayInputController.cs
@@ -6,7 +6,8 @@ using UnityEngine;
 public class GameplayInputController : MonoBehaviour
 {
     public event Action<float> OnMoveButtonPressed; 
-    public event Action<float> OnJumpButtonPressed;
+    public event Action OnJumpButtonPressed;
+    public event Action OnJumpButtonReleased;
     public event Action OnLeftClick;
     public event Action OnRightClick;
 
@@ -17,14 +18,16 @@ public class GameplayInputController : MonoBehaviour
 
         // Right Click event:
         if (Input.GetMouseButtonDown(2)) OnRightClick?.Invoke();
-    }
 
-    private void FixedUpdate()
-    {
         // Moving event:
         if (Input.GetAxis("Horizontal") != 0) OnMoveButtonPressed?.Invoke(Input.GetAxis("Horizontal"));
 
         // Jumping event:
-        if (Input.GetAxis("Jump") != 0) OnJumpButtonPressed?.Invoke(Input.GetAxis("Jump"));
+        // GetkeyUp
+        // It is not guaranteed that Input.GetKeyDown() or Input.GetKeyUp() will return true in FixedUpdate() even if the player actually done an action. Docs says:
+        // You need to call this function from the Update function, since the state gets reset each frame.It will not return true until the user has released the key and pressed it again.
+        if (Input.GetKeyUp(KeyCode.Space)) OnJumpButtonReleased?.Invoke();
+        if (Input.GetKey(KeyCode.Space)) OnJumpButtonPressed?.Invoke();
     }
+
 }
