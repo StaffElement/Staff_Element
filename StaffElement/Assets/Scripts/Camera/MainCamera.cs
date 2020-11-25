467e39a24f3dc01713b922897a91ace0e1ea7b84
ScottXileChen <53885585+ScottXileChen@users.noreply.github.com>
2020-11-25 21:04:33 +0800

    Xile chen (#9)
    * Xile Chen
    
    Updated playermovement script.
    
    * Xile Chen
    
    - Updated PlayerMovement script.
    - Added PlayerAnimation for moving and jumping.
    - Added MainCamera script to control main camera to follow target.
    
    * Xile Chen
    
    - Updated GameScene.

---
+++ StaffElement/Assets/Scripts/Camera/MainCamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCamera : MonoBehaviour
+{
+    [SerializeField]
+    private Transform followTarget;
+
+    private Vector3 offset;
+
+    private void Awake()
+    {
+        offset = transform.position - followTarget.position;    
+    }
+
+    private void LateUpdate()
+    {
+        transform.position = offset + followTarget.position;
+    }
+}
--- StaffElement/Assets/Scripts/Player/GameplayInputController.cs
+++ StaffElement/Assets/Scripts/Player/GameplayInputController.cs
@@ -5,11 +5,18 @@ using UnityEngine;
 
 public class GameplayInputController : MonoBehaviour
 {
-    public event Action<float> OnMoveButtonPressed; 
+    public event Action<float> OnMoveButtonPressed;
+    //public bool IsMoveKeyPressed { get => Input.GetAxis("Horizontal") != 0; }
+    public bool IsMoveKeyPressed { get => (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D)); }
+    public bool IsMoveKeyRelease { get => Input.GetAxis("Horizontal") == 0; }
+
     public event Action OnJumpButtonPressed;
     public event Action OnJumpButtonReleased;
+
     public event Action OnLeftClick;
     public event Action OnRightClick;
+
+    [SerializeField] KeyCode moveJumpKey;
 
     private void Update()
     {
@@ -26,8 +33,8 @@ public class GameplayInputController : MonoBehaviour
         // GetkeyUp
         // It is not guaranteed that Input.GetKeyDown() or Input.GetKeyUp() will return true in FixedUpdate() even if the player actually done an action. Docs says:
         // You need to call this function from the Update function, since the state gets reset each frame.It will not return true until the user has released the key and pressed it again.
-        if (Input.GetKeyUp(KeyCode.Space)) OnJumpButtonReleased?.Invoke();
         if (Input.GetKey(KeyCode.Space)) OnJumpButtonPressed?.Invoke();
+        if (Input.GetKeyUp(KeyCode.Space)) OnJumpButtonReleased?.Invoke();
     }
 
 }
